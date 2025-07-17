11eded76e38a823b80d444e36159bcd89af28925
Ram kinkar <ram.sb.kinkar@gmail.com>
2025-07-17 22:31:18 +0530

    SPA fallback: rewrite 403 to index.html

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -94,6 +94,11 @@ resource "aws_cloudfront_distribution" "frontend" {
     response_code         = 200
     response_page_path    = "/index.html"
   }
+    custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
 
   restrictions {
     geo_restriction {
